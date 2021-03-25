0d417cbeca0c2df9b31fc57bfb94df3d2293687c
Ben C <ben.cossum@gmail.com>
2021-03-25 13:22:51 -0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var element = React.createElement('div', null, React.createElement('h1', null, 'Hello Dojo!'),
+  React.createElement('h2', null, 'Things I need to do:'),
+  React.createElement('ul', null, React.createElement('li', null, "Learn React"), 
+    React.createElement('li', null, "Climb Mt. Everest"), 
+    React.createElement('li', null, "Run a marathon"), 
+    React.createElement('li', null, "Feed the dogs")));
+
+ReactDOM.render(element, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
