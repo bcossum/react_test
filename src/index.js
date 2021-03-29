545a0458d14f14d3b2ac9eec31611a32201a1c2a
Ben C <ben.cossum@gmail.com>
2021-03-29 13:42:45 -0700

    updated to JSX

--- src/index.js
+++ src/index.js
@@ -5,14 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import reactDom from 'react-dom';
 
-var element = React.createElement('div', null, React.createElement('h1', null, 'Hello Dojo!'),
-  React.createElement('h2', null, 'Things I need to do:'),
-  React.createElement('ul', null, React.createElement('li', null, "Learn React"), 
-    React.createElement('li', null, "Climb Mt. Everest"), 
-    React.createElement('li', null, "Run a marathon"), 
-    React.createElement('li', null, "Feed the dogs")));
+// var element = React.createElement('div', null, React.createElement('h1', null, 'Hello Dojo!'),
+//   React.createElement('h2', null, 'Things I need to do:'),
+//   React.createElement('ul', null, React.createElement('li', null, "Learn React"), 
+//     React.createElement('li', null, "Climb Mt. Everest"), 
+//     React.createElement('li', null, "Run a marathon"), 
+//     React.createElement('li', null, "Feed the dogs")));
 
-ReactDOM.render(element, document.getElementById('root'));
+function Hello(props) {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <p>Now we are getting somerwhere</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
 
 
 // If you want to start measuring performance in your app, pass a function
